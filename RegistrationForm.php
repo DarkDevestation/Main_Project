38ed125d8ef4703e5c56ffd199c22060d14edfee
Brandon Forster <brandonforster@gmail.com>
2014-04-06 16:43:19 -0400

    fix html

--- RegistrationForm.php
+++ RegistrationForm.php
@@ -3,8 +3,8 @@
 <body>
 <p>Residence Verification *</p><!--<br>-->
 <form action="general form.php" method="post">
-<input type="radio" name="rv1" value="yes">Yes<br>
-<input type="radio" name="rv2" value="no">No<br>
+<input type="radio" name="resVer" value="true">Yes<br>
+<input type="radio" name="resVer" value="false">No<br>
 <br>
 Type of household *<br><select name="Type of Household">
 <option value="single">Single Household</option>
@@ -18,18 +18,18 @@ Zip Code: <input type="text" name="zipcode"><br>
 E-mail: <input type="text" name="email"><br>
 Primary Phone: <input type="text" name="primaryphone"><br>
 
-<input type="radio" name="home1" value="home1">Home<br>
-<input type="radio" name="cell1" value="cell1">Cell<br>
-<input type="radio" name="work1" value="work1">Work<br>
-<input type="radio" name="other1" value="other1">Other:<br>
+<input type="radio" name="phoneTypePrimary" value="home1">Home<br>
+<input type="radio" name="phoneTypePrimary" value="cell1">Cell<br>
+<input type="radio" name="phoneTypePrimary" value="work1">Work<br>
+<input type="radio" name="phoneTypePrimary" value="other1">Other:<br>
 <input type="text" name="othertext1"><br>
 
 Secondary Phone: <input type="text" name="secondaryphone"><br>
 
-<input type="radio" name="home2" value="home2">Home<br>
-<input type="radio" name="cell2" value="cell2">Cell<br>
-<input type="radio" name="work2" value="work2">Work<br>
-<input type="radio" name="other2" value="other2">Other:<br>
+<input type="radio" name="phoneTypeSecondary" value="home2">Home<br>
+<input type="radio" name="phoneTypeSecondary" value="cell2">Cell<br>
+<input type="radio" name="phoneTypeSecondary" value="work2">Work<br>
+<input type="radio" name="phoneTypeSecondary" value="other2">Other:<br>
 <input type="text" name="othertext1"><br>
 
 Number of Family Members<input type="number" name="othertext1"><br>
@@ -43,12 +43,12 @@ Language Spoken* <br><select name="Language Spoken">
 <option value="spanish">Spanish</option>
 </select><br>
 
-Delivery<br><input type="radio" name="delivery1" value="yes">Yes<br>
-<input type="radio" name="delivery2" value="no">No<br>
+Delivery<br><input type="radio" name="delivery" value="true">Yes<br>
+<input type="radio" name="delivery" value="false">No<br>
 <p>See Anne of Maryann if yes</p>
 
-Christmas Store selection*<br><input type="radio" name="css1" value="yes">Yes<br>
-<input type="radio" name="css2" value="no">No<br>
+Christmas Store selection*<br><input type="radio" name="storeSelection" value="food">Food<br>
+<input type="radio" name="storeSelection" value="toys">Clothing & Toys<br>
 <p>(Clothing & Toys is for children 12 & under)</p>
 
 How did you learn about the Stores?* <br><select name="Learn">
@@ -59,8 +59,8 @@ How did you learn about the Stores?* <br><select name="Learn">
 </select><br>
 
 <b>Can a member of the St. Margaret Mary Church and Community Organization call you after the holidays to talk more about the needs and concerns of you and your family?*</b>
-<br><input type="radio" name="can1" value="yes">Yes<br>
-<input type="radio" name="can2" value="no">No<br>
+<br><input type="radio" name="contact" value="true">Yes<br>
+<input type="radio" name="contact" value="false">No<br>
 <p>(Clothing & Toys is for children 12 & under)</p>
 
 Notes: <input type="text" name="notes"><br>
