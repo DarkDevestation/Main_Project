19334b1dc6811c7fbd260b6f8ebb7e4088bcac2c
DarkDevestation <blackdice0123@gmail.com>
2014-04-06 16:36:59 -0400

    Changes
    2Pac

--- RegistrationForm.php
+++ RegistrationForm.php
@@ -3,12 +3,14 @@
 <body>
 <p>Residence Verification *</p><!--<br>-->
 <form action="general form.php" method="post">
-<input type="radio" name="rv1" value="yes">Yes<br>
-<input type="radio" name="rv2" value="no">No<br>
+<input type="radio" name="rv"<?php if (isset($rv) && $rv=="1") echo "checked";?>
+value="1">>Yes<br>
+<input type="radio" name="rv" <?php if (isset($rv) && $rv=="0") echo "checked";?>
+value="0">>No<br>
 <br>
 Type of household *<br><select name="Type of Household">
-<option value="single">Single Household</option>
-<option value="combined">Combined Household</option>
+<option value="0">Single Household</option>
+<option value="1">Combined Household</option>
 </select><br>
 Address: <input type="text" name="address1"><br>
 Address2: <input type="text" name="address2"><br>
@@ -18,21 +20,29 @@ Zip Code: <input type="text" name="zipcode"><br>
 E-mail: <input type="text" name="email"><br>
 Primary Phone: <input type="text" name="primaryphone"><br>
 
-<input type="radio" name="home1" value="home1">Home<br>
-<input type="radio" name="cell1" value="cell1">Cell<br>
-<input type="radio" name="work1" value="work1">Work<br>
-<input type="radio" name="other1" value="other1">Other:<br>
-<input type="text" name="othertext1"><br>
+<input type="radio" name="phone1" <?php if (isset($phone1) && $phone1=="0") echo "checked";?>
+value="0">>Home<br>
+<input type="radio" name="phone1" <?php if (isset($phone1) && $phone1=="1") echo "checked";?>
+value="1">>Cell<br>
+<input type="radio" name="phone1" <?php if (isset($phone1) && $phone1=="2") echo "checked";?>
+value="2">>Work<br>
+<input type="radio" name="phone1" <?php if (isset($phone1) && $phone1=="3") echo "checked";?>
+value="3">>Other:<br>
+<input type="number" name="othertext1"><br>
 
 Secondary Phone: <input type="text" name="secondaryphone"><br>
 
-<input type="radio" name="home2" value="home2">Home<br>
-<input type="radio" name="cell2" value="cell2">Cell<br>
-<input type="radio" name="work2" value="work2">Work<br>
-<input type="radio" name="other2" value="other2">Other:<br>
-<input type="text" name="othertext1"><br>
+<input type="radio" name="phone2" <?php if (isset($phone2) && $phone2=="0") echo "checked";?>
+value="0">>Home<br>
+<input type="radio" name="phone2" <?php if (isset($phone2) && $phone2=="1") echo "checked";?>
+value="1">>Cell<br>
+<input type="radio" name="phone2" <?php if (isset($phone2) && $phone2=="2") echo "checked";?>
+value="2">>Work<br>
+<input type="radio" name="phone2" <?php if (isset($phone2) && $phone2=="3") echo "checked";?>
+value="3">>Other:<br>
+<input type="number" name="othertext2"><br>
 
-Number of Family Members<input type="number" name="othertext1"><br>
+Number of Family Members<input type="number" name="nummembers"><br>
 Language Spoken* <br><select name="Language Spoken">
 <option value="english">English</option>
 <option value="spanish">Spanish</option>
@@ -59,24 +69,29 @@ How did you learn about the Stores?* <br><select name="Learn">
 </select><br>
 
 <b>Can a member of the St. Margaret Mary Church and Community Organization call you after the holidays to talk more about the needs and concerns of you and your family?*</b>
-<br><input type="radio" name="can1" value="yes">Yes<br>
-<input type="radio" name="can2" value="no">No<br>
+<br><input type="radio" name="can" <?php if (isset($can) && $can=="1") echo "checked";?>
+value="1">Yes<br>
+<input type="radio" name="can" <?php if (isset($can) && $can=="0") echo "checked";?>
+value="0">No<br>
 <p>(Clothing & Toys is for children 12 & under)</p>
 
 Notes: <input type="text" name="notes"><br>
 This Form was completed by* <input type="text" name="completed"><br>
-
-<input type="submit" value="Submit">
+<input type="submit" name="submit">
 </form>
-<?php
-// Create connection
- $con=mysqli_connect("localhost");
 
-// Check connection
- if (mysqli_connect_errno())
-   {
-   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-   }
- ?> 
 </body>
 </html>
+<?php 
+if(isset($_GET['submit']))
+{ 
+	$con = mysql_connect("127.0.0.1");
+    mysql_select_db("stmargaretmarycatholicchurch", $con);
+    $sql = "SELECT COUNT(*)"
+    $sql ="INSERT INTO household(residenceverification, residenceverficationnote, idhouseholdtype, address1, address2, city, state, zipcode
+           email, idphonetypeprimary, phonenumberprimary, idphonetypesecondary, phonenumnersecondary, numberoffamilymembers, idlanguage)
+           VALUES
+           ('$_GET[rv]','$_GET[notes]'')";
+           mysql_close($con);
+} 
+?>
